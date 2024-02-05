14b6cc12edd45575afbb7a1447783278daac90c6
Thomasperge <thomaskauffmant74@gmail.com>
2024-02-05 12:09:09 +0100

    feat: adding screen

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print('hello world')
+import pygame
+
+# Screen
+pygame.init()
+width = 600
+height = 600
+screen = pygame.display.set_mode((width, height))
+background = (255, 255, 255)
+blue = (0, 0, 255)
+red = (255, 0, 0)
+screen.fill(background)
+
+game_started = True
+clock = pygame.time.Clock()
+
+while game_started :
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_started = False
+
+pygame.quit()
