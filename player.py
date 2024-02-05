6527815341a2fafb82120cb0d43cb9540e4e0192
Thomasperge <thomaskauffmant74@gmail.com>
2024-02-05 11:35:45 +0100

    feat: adding player

---
+++ player.py
@@ -0,0 +1,10 @@
+class Player :
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_width(self):
+        return self.width
+    
+    def get_height(self):
+        return self.height
